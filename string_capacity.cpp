88e9c1f4669f841c455b4a1a598c5f206189fff6
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-27 10:47:05 +0600

    empty or not

---
+++ string_capacity.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "hello World";
+    // s.clear();
+    if(s.empty()== true)
+    {
+        cout << "empty"<< endl;
+    }
+    else
+    {
+        cout << "not empty" << endl;
+    }
+    return 0;
+} 
