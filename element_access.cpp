e7131d168e4e6f29b9565bee1c754fb5f08bede7
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-27 11:02:29 +0600

    olternative of back and front function

--- element_access.cpp
+++ element_access.cpp
@@ -7,6 +7,7 @@ int main()
     cout << s[1] << endl;
     cout << s.at(0) << endl;
     cout << s.back() << endl;
-    cout << s.front() << endl;
+    cout << s[s.size()-1] << endl; // olternative of back function
+    cout << s.front() << endl; // olternative is s[0]
     return 0;
 }
