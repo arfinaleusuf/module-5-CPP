3bb32ceb4204f1d7e79329df09aca8b55d513270
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-27 11:06:53 +0600

    append function

---
+++ string_modifiers.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "hello world";
+    string s2 = "hello";
+    // s += s2;
+    s.append(s2);
+    cout << s << endl;
+    return 0;
+}
