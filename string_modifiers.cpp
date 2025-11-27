b02399507ed1e7e3f16c39cb7a847acab2cc5405
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-27 11:09:07 +0600

    olternarive of push back

--- string_modifiers.cpp
+++ string_modifiers.cpp
@@ -5,7 +5,8 @@ int main()
     string s = "hello world";
     // s += s2;
     // s.append(s2);
-    s.push_back('a');
+    // s.push_back('a');
+    s += 'a';
     cout << s << endl;
     return 0;
 }
